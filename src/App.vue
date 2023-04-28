a3699ba668c13553c688b28dbd0aef5f85d10423
Chon Lam <1685020647@qq.com>
2023-04-29 07:50:32 +0800

    feat: add header interactive

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
+import { routes } from './router/index'
+
+const routesRef = ref(routes.map(item => {
+  return {
+    ...item,
+    active: item.name === 'Blog'
+  }
+}))
+
+function handlePageNav(activeIndex: number) {
+  routesRef.value.forEach((item, curindex) => item.active = activeIndex === curindex)
+}
+
 </script>
 
 <template>
   <header>
     <span>BLOGKLIN</span>
-    <nav>
-      <a href="#">Blog</a>
-      <a href="#">Tool</a>
-      <a href="#">About</a>
+    <nav v-if="routesRef.length > 0">
+      <a
+        :class="{'active-page': item.active}"
+        v-for="(item, index) in routesRef"
+        @click="handlePageNav(index)"
+        :key="item.name"
+      >{{ item.name }}</a>
     </nav>
   </header>
 
@@ -36,5 +53,16 @@ import { RouterLink, RouterView } from 'vue-router'
   a {
     color: #fff;
     text-decoration: none;
+    user-select: none;
+    transition: color 200ms ease 0s;
+    cursor: pointer;
+  }
+
+  a:hover {
+    color: rgb(92, 198, 199);
+  }
+
+  .active-page {
+    color: rgb(51, 147, 148);
   }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import BlogView from '../views/BlogView.vue'
+
+export const routes = [
+  {
+    path: '/',
+    name: 'Blog',
+    component: BlogView
+  },
+  {
+    path: '/tool',
+    name: 'Tool',
+    component: () => import('../views/ToolView.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 export default router
