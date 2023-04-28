9759382d53ad865ce665e5373a3ca9c77623a4e7
Chon Lam <1685020647@qq.com>
2023-04-29 07:32:17 +0800

    feat: add font

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,37 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-
+    <span>BLOGKLIN</span>
+    <nav>
+      <a href="#">Blog</a>
+      <a href="#">Tool</a>
+      <a href="#">About</a>
+    </nav>
   </header>
 
   <!--<RouterView />-->
 </template>
 
 <style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px;
+  }
 
+  span {
+    font-weight: bold;
+    letter-spacing: 3px;
+  }
+
+  nav {
+    display: flex;
+    gap: 30px;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
 </style>
