66a84e6dd8e49a4591b9b0cd6c6db19b55ec9b40
Chon Lam <1685020647@qq.com>
2023-05-01 20:38:44 +0800

    chore: add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Markdown()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
