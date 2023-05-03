6ccf60fac7acb521678c0576bbb80d6f59f1ff46
Chon Lam <1685020647@qq.com>
2023-05-03 08:40:55 +0800

    feat: add introdution

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,100 @@
+<script setup lang="ts">
+  const introduction = `BLOGKLIN is Chon W. Lam's space for sharing and demonstrating frontend techniques. Creating a personal blog is a way to unleash the power of the web that is restricted by dot-com companies.`
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="introduction">
+      <span id="introd" tg-name="fade" class="title">{{ introduction }}</span>
+      <span class="logo">B</span>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style scoped>
+
+.about {
+  height: 200vh;
+}
+
+.introduction {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 100px;
+  padding: 90px 32px;
+}
+
+.introduction .title {
+  width: 500px;
+  font-size: 40px;
+  line-height: 48px;
+  font-weight: bold;
+  color: rgb(92, 198, 199);
+}
+
+.introduction .logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  border: 15px solid #fff;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 8em;
+  color: #fff;
+  letter-spacing: -7px;
+  font-weight: 700;
+  text-transform: uppercase;
+  animation: blur 3s ease-out infinite;
+  text-shadow: 0px 0px 5px #fff, 0px 0px 7px #fff;
+  box-shadow: 0px 0px 5px #fff, 0px 0px 7px #fff;
+}
+
+@keyframes blur {
+  from {
+    text-shadow:0px 0px 10px #fff,
+      0px 0px 10px #fff, 
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 50px #fff,
+      0px 0px 50px #fff,
+      0px 0px 50px #7B96B8,
+      0px 0px 150px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px -10px 100px #7B96B8,
+      0px -10px 100px #7B96B8;
+
+      box-shadow:0px 0px 10px #fff,
+      0px 0px 10px #fff, 
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 50px #fff,
+      0px 0px 50px #fff,
+      0px 0px 50px #7B96B8,
+      0px 0px 150px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px -10px 100px #7B96B8,
+      0px -10px 100px #7B96B8;
   }
 }
+
+#introd {
+  opacity: calc(var(--fade) * 1);
+}
+
 </style>
