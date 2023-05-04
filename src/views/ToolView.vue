b5da008b308203ce11e8b2148f84a0526dfa92f1
Dominguito <1685020647@qq.com>
2023-05-04 10:31:27 +0800

    feat: add light effect in tools

--- src/views/ToolView.vue
+++ src/views/ToolView.vue
@@ -31,10 +31,24 @@
   }
 
   .tool-item {
+    position: relative;
     display: flex;
     flex-direction: column;
     max-height: 150px;
     padding: 10px 20px;
+    transition: box-shadow .75s ease;
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  .tool-item:hover {
+    background: rgb(225, 113, 139);
+    color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgb(225, 113, 139),
+                0 0 25px rgb(225, 113, 139),
+                0 0 50px rgb(225, 113, 139),
+                0 0 100px rgb(225, 113, 139);
   }
 
   .tool-item header {
@@ -59,4 +73,85 @@
     align-items: center;
     flex: 1
   }
+
+  .tool-item .animation {
+    position: absolute;
+    display: block;
+  }
+
+  .tool-item .animation:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, rgb(225, 113, 139));
+  animation: btn-anim1 1s linear infinite;
+  }
+
+  @keyframes btn-anim1 {
+    0% {
+      left: -100%;
+    }
+    50%,100% {
+      left: 100%;
+    }
+  }
+
+  .tool-item .animation:nth-child(2) {
+    top: -100%;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(180deg, transparent, rgb(225, 113, 139));
+    animation: btn-anim2 1s linear infinite;
+    animation-delay: .25s
+  }
+
+  @keyframes btn-anim2 {
+    0% {
+      top: -100%;
+    }
+    50%,100% {
+      top: 100%;
+    }
+  }
+
+  .tool-item .animation:nth-child(3) {
+    bottom: 0;
+    right: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(270deg, transparent, rgb(225, 113, 139));
+    animation: btn-anim3 1s linear infinite;
+    animation-delay: .5s
+  }
+
+  @keyframes btn-anim3 {
+    0% {
+      right: -100%;
+    }
+    50%,100% {
+      right: 100%;
+    }
+  }
+
+  .tool-item .animation:nth-child(4) {
+    bottom: -100%;
+    left: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(360deg, transparent, rgb(225, 113, 139));
+    animation: btn-anim4 1s linear infinite;
+    animation-delay: .75s
+  }
+
+  @keyframes btn-anim4 {
+    0% {
+      bottom: -100%;
+    }
+    50%,100% {
+      bottom: 100%;
+    }
+  }
+
 </style>
