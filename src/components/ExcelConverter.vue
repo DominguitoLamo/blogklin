07afb8b755b555f0659ef7735ed82c0c495d63f0
DominguitoLamo <1685020647@qq.com>
2023-05-05 09:53:53 +0800

    feat: add to buttons

--- src/components/ExcelConverter.vue
+++ src/components/ExcelConverter.vue
@@ -12,13 +12,56 @@ function handleUploadChange(file: File) {
 </script>
 <template>
   <div class="converter">
-    <div class="upload">
-      <UploadFile @change="handleUploadChange" file-type="xlsx" />
+    <div class="upload-box">
+      <div class="upload">
+        <UploadFile @change="handleUploadChange" file-type="xlsx" />
+      </div>
+      <div class="buttons">
+        <button class="normal-button json-btn">To Json</button>
+        <button class="normal-button md-btn">To Md</button>
+      </div>
+      <div class="preview">
+
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
-.upload {
+.upload-box {
   padding-left: 14px;
 }
+
+.upload {
+  margin-bottom: 14px;
+}
+
+.buttons {
+  display: flex;
+  gap: 12px;
+  padding-left: 10px;
+}
+
+.normal-button.json-btn {
+  background: rgb(6,14,131);
+  background: linear-gradient(0deg, rgba(6,14,131,1) 0%, rgba(12,25,180,1) 100%);
+  border: none;
+}
+
+.normal-button.json-btn:hover {
+  background: rgb(0,3,255);
+  background: linear-gradient(0deg, rgba(0,3,255,1) 0%, rgba(2,126,251,1) 100%);
+}
+
+.normal-button.md-btn {
+  background: rgba(251,33,117,1);
+  background: linear-gradient(0deg, rgba(251,33,117,1) 0%, rgba(234,76,137,1) 100%);
+  border: none;
+}
+
+.normal-button.md-btn:hover {
+  background: rgb(238, 94, 149);
+  background: linear-gradient(0deg, rgb(238, 94, 149) 0%, rgba(234,76,137,1) 100%);
+  border: none;
+}
 </style>
+
